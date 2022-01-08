23dc0bcf0a6b9609aa4ce227f094d040b5db59ce
Yehor-ka <kalolg0109@gmail.com>
2022-01-08 22:12:27 +0200

    first commit

---
+++ src/components/Controls.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Search from './Search'
+import { CustomSelect } from './CustomSelect'
+import styled from 'styled-components'
+
+
+const options = [
+    {value: 'Arfica', label: 'Arfica'},
+    {value: 'America', label: 'America'},
+    {value: 'Asia', label: 'Asia'},
+    {value: 'Europe', label: 'Europe'},
+    {value: 'Oceania', label: 'Oceania'},
+]
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    @media (min-width: 767px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+`
+
+const Controls = () => {
+    const [search, setSearch] = React.useState('')
+
+    const [region, setRegion] = React.useState('')
+    return (
+        <Wrapper>
+            <Search search={search} setSearch={setSearch} />  
+            <CustomSelect options={options} placeholder="Filter by region" isClearable isSearchable={false} value={region} onChange={setRegion} /> 
+        </Wrapper>
+    )
+}
+
+export default Controls
---
+++ src/components/Header.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+
+import { Container } from './Container';
+
+const HeaderEl = styled.header`
+  box-shadow: var(--shadow);
+  background-color: var(--colors-ui-base);
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 0;
+`;
+
+const Title = styled.a.attrs({
+  href: '/',
+})`
+    color: var(--colors-text);
+    font-size: var(--fs-sm);
+    text-decoration: none;
+    font-weight: var(--fw-bold);
+`;
+
+const ModeSwitcher = styled.div`
+    color: var(--color-text);
+    font-size: var(--fs-sm);
+    cursor: pointer;
+    //font-weight: var(--fw-bold);
+`;
+
+const Header = () => {
+  const [theme, setTheme] = React.useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  React.useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
+    <HeaderEl>
+      <Container>
+        <Wrapper>
+          <Title>Where is the world?</Title>
+          <ModeSwitcher onClick={toggleTheme}>
+            {theme === 'light' ? (
+              <>
+                <IoMoonOutline />
+                <span style={{marginLeft: '.75rem'}}>Light Theme</span>
+              </>
+            ) : (
+              <>
+                <IoMoon />
+                <span style={{marginLeft: '.75rem'}}>Dark Theme</span>
+              </>
+            )}
+          </ModeSwitcher>
+        </Wrapper>
+      </Container>
+    </HeaderEl>
+  );
+};
+
+export default Header;
